f22df6f8045b12bd0add7fb4dd9ee28eeea8b35b
Hannes Hofmeister <Hannes.hofmeister@uni-ulm.de>
2024-04-20 05:12:22 +0200

    feat: Player can shoot

--- TeamNein/Player.cs
+++ TeamNein/Player.cs
@@ -3,43 +3,72 @@ using System;
 
 public partial class Player : Area2D
 {
-	[Export] public int Speed { get; set; } = 300;
-	public Vector2 ScreenSize;
-	
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		ScreenSize = GetViewportRect().Size;
-	}
+    [Export] PackedScene bullet_scn; //Insert Scene from Godot
+    //Shooting Attributes
+    private float _fire_rate;
+    private float _time_untile_fire = 0f;
+    //Moving Attributes 
+    [Export] public int Speed = 300;
+    public Vector2 ScreenSize;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		var velocity = Vector2.Zero; // The player's movement vector.
-		if (Input.IsActionPressed("move_right")) {
-			velocity.X += 1;
-		}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        ScreenSize = GetViewportRect().Size;
+        _fire_rate = 0.5f;
+    }
 
-		if (Input.IsActionPressed("move_left")){
-			velocity.X -= 1;
-		}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        //Movment Inputs
+        var velocity = Vector2.Zero; // The player's movement vector.
+        if (Input.IsActionPressed("move_right"))
+        {
+            velocity.X += 1;
+        }
 
-		if (Input.IsActionPressed("move_down")){
-			velocity.Y += 1;
-		}
+        if (Input.IsActionPressed("move_left"))
+        {
+            velocity.X -= 1;
+        }
 
-		if (Input.IsActionPressed("move_up")){
-			velocity.Y -= 1;
-		}
-		if (velocity.Length() > 0)
-		{
-			velocity = velocity.Normalized() * Speed;
-		}
-		
-		Position += velocity * (float)delta;
-		Position = new Vector2(
-			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
-			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
-		);
-	}
+        if (Input.IsActionPressed("move_down"))
+        {
+            velocity.Y += 1;
+        }
+
+        if (Input.IsActionPressed("move_up"))
+        {
+            velocity.Y -= 1;
+        }
+        //Diagonal Movement has the same speed as straight Movement
+        if (velocity.Length() > 0)
+        {
+            velocity = velocity.Normalized() * Speed;
+        }
+
+        //Make it smooth 
+        Position += velocity * (float)delta;
+        Position = new Vector2(
+            x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+            y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+        );
+
+
+        Rotation = (GetGlobalMousePosition() - GlobalPosition).Angle(); //Player looks to the mouse 
+        if (Input.IsActionPressed("shoot") && _time_untile_fire > _fire_rate)
+        {
+            _time_untile_fire = 0f;
+            Node2D bullet = bullet_scn.Instantiate<Node2D>();
+            bullet.Rotation = GlobalRotation;
+            bullet.GlobalPosition = GlobalPosition;
+
+            GetTree().Root.AddChild(bullet);
+        }
+        else
+        {
+            _time_untile_fire += (float)delta;
+        }
+    }
 }
