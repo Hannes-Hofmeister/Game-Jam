266b898d4f2f5bddbe853a8b9f199c34a7ad2769
Hannes Hofmeister <Hannes.hofmeister@uni-ulm.de>
2024-04-20 21:34:54 +0200

    feat: Enemy has health and dies after taking damage

--- TeamNein/Scripts/enemys.gd
+++ TeamNein/Scripts/enemys.gd
@@ -3,11 +3,13 @@ extends CharacterBody2D
 var speed = 25
 var playerChase = false
 var player = null
+var health = 100
 
 func _physics_process(delta):
 	if playerChase:
 			position += (player.position - position)/speed
-			
+	if health<0:
+			queue_free()		
 			
 			
 
@@ -16,10 +18,16 @@ func _physics_process(delta):
 func _on_detection_area_body_entered(body):
 	player = body
 	playerChase = true
+	
 	pass # Replace with function body.
 
 
 func _on_detection_area_body_exited(body):
 	player = null
 	playerChase = false
+	pass # Replace with function body.
+
+
+func _on_area_2d_area_entered_bullet(area):
+	health += -40
 	pass # Replace with function body.
