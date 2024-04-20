a5ecd3b43d6b65bdfaf39b765f59012670745e01
Volkiisvolk <volkan.uygun@uni-ulm.de>
2024-04-20 14:02:24 +0200

    fix: Godot can be started again

--- TeamNein/Scripts/Player.cs
+++ TeamNein/Scripts/Player.cs
@@ -3,6 +3,7 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
+	
 	[Export] PackedScene bullet_scn; //Insert Scene from Godot
 	//Shooting Attributes
 	private float _fire_rate;
@@ -14,6 +15,7 @@ public partial class Player : CharacterBody2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Node2D bullet = bullet_scn.Instantiate<Node2D>();
 		ScreenSize = GetViewportRect().Size;
 		_fire_rate = 0.5f;
 	}
@@ -48,59 +50,58 @@ public partial class Player : CharacterBody2D
 			velocity = velocity.Normalized() * Speed;
 		}
 
-<<<<<<< HEAD
-        //Make it smooth 
-        Position += velocity * (float)delta;
-        Position = new Vector2(
-            x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
-            y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
-        );
-        
+		//Make it smooth 
+		Position += velocity * (float)delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+		);
+		
 
 
-        //Rotation = (GetGlobalMousePosition() - GlobalPosition).Angle(); //Player looks to the mouse 
-        
-        var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-        if (velocity is { X: 0, Y: < 0 })
-        {
-            animatedSprite2D.Animation = "walk up";
-        }
-        else if (velocity is { X: > 0, Y: 0 })
-        { 
-            animatedSprite2D.Animation = "walk right";
-        }
-        else if (velocity is { X: 0, Y: >0 })
-        { 
-            animatedSprite2D.Animation = "walk down";
-        }
-        else if (velocity is { X: < 0, Y: 0 })
-        { 
-            animatedSprite2D.Animation = "walk left";
-        }
-        else if (velocity is { X: < 0, Y: >0 })
-        { 
-            animatedSprite2D.Animation = "walk down left";
-        }
-        else if (velocity is { X: > 0, Y: >0 })
-        { 
-            animatedSprite2D.Animation = "walk down right";
-        }
-        else if (velocity is { X: < 0, Y: <0 })
-        { 
-            animatedSprite2D.Animation = "walk down left";
-        }
-        else if (velocity is { X: > 0, Y: <0 })
-        { 
-            animatedSprite2D.Animation = "walk up right";
-        }
-       
-        if (Input.IsActionPressed("shoot") && _time_untile_fire > _fire_rate)
-        {
-            _time_untile_fire = 0f;
-            Node2D bullet = bullet_scn.Instantiate<Node2D>();
-            bullet.Rotation = GlobalRotation;
-            bullet.GlobalPosition = GlobalPosition;
-=======
+		//Rotation = (GetGlobalMousePosition() - GlobalPosition).Angle(); //Player looks to the mouse 
+		
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		if (velocity is { X: 0, Y: < 0 })
+		{
+			animatedSprite2D.Animation = "walk up";
+		}
+		else if (velocity is { X: > 0, Y: 0 })
+		{ 
+			animatedSprite2D.Animation = "walk right";
+		}
+		else if (velocity is { X: 0, Y: >0 })
+		{ 
+			animatedSprite2D.Animation = "walk down";
+		}
+		else if (velocity is { X: < 0, Y: 0 })
+		{ 
+			animatedSprite2D.Animation = "walk left";
+		}
+		else if (velocity is { X: < 0, Y: >0 })
+		{ 
+			animatedSprite2D.Animation = "walk down left";
+		}
+		else if (velocity is { X: > 0, Y: >0 })
+		{ 
+			animatedSprite2D.Animation = "walk down right";
+		}
+		else if (velocity is { X: < 0, Y: <0 })
+		{ 
+			animatedSprite2D.Animation = "walk down left";
+		}
+		else if (velocity is { X: > 0, Y: <0 })
+		{ 
+			animatedSprite2D.Animation = "walk up right";
+		}
+	   
+		if (Input.IsActionPressed("shoot") && _time_untile_fire > _fire_rate)
+		{
+			_time_untile_fire = 0f;
+			Node2D bullet = bullet_scn.Instantiate<Node2D>();
+			bullet.Rotation = GlobalRotation;
+			bullet.GlobalPosition = GlobalPosition;
+
 		//Make it smooth 
 		Position += velocity * (float)delta;
 		Position = new Vector2(
@@ -113,10 +114,9 @@ public partial class Player : CharacterBody2D
 		if (Input.IsActionPressed("shoot") && _time_untile_fire > _fire_rate)
 		{
 			_time_untile_fire = 0f;
-			Node2D bullet = bullet_scn.Instantiate<Node2D>();
 			bullet.Rotation = GlobalRotation;
 			bullet.GlobalPosition = GlobalPosition;
->>>>>>> 35f4eeda96796d7a759f4877c55ccdf791ce88a1
+
 
 			GetTree().Root.AddChild(bullet);
 		}
@@ -125,4 +125,5 @@ public partial class Player : CharacterBody2D
 			_time_untile_fire += (float)delta;
 		}
 	}
+}
 }
