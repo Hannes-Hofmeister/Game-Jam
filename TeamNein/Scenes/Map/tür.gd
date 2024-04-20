12cee613731f251f9ed5228980430d04887f1dab
Hechtei <hechtei0@gmail.com>
2024-04-20 23:32:13 +0200

    Shop Kann Betreten werden

--- TeamNein/Scenes/Map/tür.gd
+++ TeamNein/Scenes/Map/tür.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
-signal shopDoorEntered;
+var shop = preload("res://Scenes/Map/shop_room.tscn").instantiate();
+var player = preload("res://Entities/player.tscn").instantiate();
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,7 +14,17 @@ func _process(delta):
 
 func _on_body_entered(body):
 	print("Test");
+	get_tree().get_root().add_child(shop)
 	var root_node = get_tree().get_root().get_children();
 	for child in root_node:
-		child.free()
+		if(child.is_in_group("NormalRoom")):
+			child.free()
+		elif(child.is_in_group("Player")):
+			child.position = Vector2(30,256)
+		
 	
+	
+	
+	
+
+
