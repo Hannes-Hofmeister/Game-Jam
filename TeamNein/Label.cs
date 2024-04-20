f3261463805b6690054aad5416c9536d8fb8a1c9
David Jenzer <david.jenzer@uni-ulm.com>
2024-04-20 12:57:17 +0200

    aenderungen fuer peterboy

--- TeamNein/Label.cs
+++ TeamNein/Label.cs
@@ -1,21 +1,22 @@
 using Godot;
 using System;
 
-public partial class Node2D : Godot.Node2D{
+public partial class Label : Godot.Label{
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready(){
 		GD.Print("whatever1");
 		var timer = GetNode<Timer>("Timer");
-		timer.Timeout -= OnTimerTimeout;
+		timer.Timeout += _on_timer_timeout();
 	}
-	
-	private void OnTimerTimeout(){
-		GD.Print("whatever");
-		Visible = !Visible;
+	private void _on_timer_timeout(){
+		Visible = !Visible();
+		GD.Print("henlo");
 	}
 	
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta){
 	}
 }
+
+
