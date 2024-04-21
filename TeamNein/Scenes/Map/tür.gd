6fe9b3144247b8c1b05600f3f9e36cc941957f53
Hannes Hofmeister <Hannes.hofmeister@uni-ulm.de>
2024-04-21 02:37:05 +0200

    tür vergessen git commit messages sind eh verloren

--- TeamNein/Scenes/Map/tür.gd
+++ TeamNein/Scenes/Map/tür.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 var shop = preload("res://Scenes/Map/shop_room.tscn").instantiate();
 var player = preload("res://Entities/player.tscn").instantiate();
+
+signal DoorActivated
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -14,6 +16,13 @@ func _process(delta):
 
 func _on_body_entered(body):
 	print("Test");
+	emit_signal("DoorActivated")
+	
+	
+	
+	
+	
+	
 	get_tree().get_root().add_child(shop)
 	var root_node = get_tree().get_root().get_children();
 	for child in root_node:
